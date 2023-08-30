dcd64e36c98619b48827bdee74065a3f395c7a7d
Nour-eddine Taleb <contact@noureddine.xyz>
2023-08-30 12:45:09 +0100

    change README

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,10 +2,21 @@
 
 set -xe
 
+function die() {
+    echo $@ >&2
+    exit 1
+}
+
 if ! command -v rustup
 then
     curl https://sh.rustup.rs | sh
 fi
+
+if ! command -v qemu-system-x86_64
+then
+    die you need to install qemu for you distribution, and rerun this again !
+fi
+
 
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 
